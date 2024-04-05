d648c91d311b619e328bd1ff6b11f9f00f109f81
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-04-05 13:42:39 +0300

    imported Header, Navbar and NavbarLine components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,16 @@
+import Header from "./components/Header"
+import Navbar from "./components/Navbar"
+import NavbarLine from "./components/NavbarLine"
+
 function App() {
-  
+  return (
+    <div className="main">
+      <Header />
+      <NavbarLine />
+      <Navbar />
+      <NavbarLine />
+    </div>
+  )
 }
 
 export default App
