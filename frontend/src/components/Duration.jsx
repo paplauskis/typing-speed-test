abe961bca194ddd3fe930806319acd85bc721ca8
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-04-05 18:18:06 +0300

    added useState to buttons for choosing length of type test

--- frontend/src/components/Duration.jsx
+++ frontend/src/components/Duration.jsx
@@ -1,13 +1,15 @@
 import { useState } from "react"
 function Duration () {
+  const [activeIndex, setActiveIndex] = useState(1)
+  console.log(activeIndex)
   return (
     <div className="duration">
       <p>Choose the duration of the test</p>
       <div className="duration-buttons">
-        <button className="duration-button">1</button>
-        <button className="duration-button">2</button>
-        <button className="duration-button">3</button>
-        <p>minute(s)</p>
+        <button className={`duration-button ${activeIndex === 1 ? 'active' : ''}`} onClick={() => setActiveIndex(1)}>1</button>
+        <button className={`duration-button ${activeIndex === 2 ? 'active' : ''}`} onClick={() => setActiveIndex(2)}>2</button>
+        <button className={`duration-button ${activeIndex === 3 ? 'active' : ''}`} onClick={() => setActiveIndex(3)}>3</button>
+        <p>{activeIndex === 1 ? 'minute' : 'minutes'}</p>
       </div>
     </div>
   )
