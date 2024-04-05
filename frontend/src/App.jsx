5172ecd09009b8ecc24c972da545b8b92aeebf57
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-04-05 18:40:17 +0300

    added useState to track duration choice

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,18 @@
 import Header from "./components/Header"
 import Navbar from "./components/Navbar"
 import NavbarLine from "./components/NavbarLine"
+import Duration from "./components/Duration"
+import { useState } from "react"
 
 function App() {
+  const [activeIndex, setActiveIndex] = useState(1)
   return (
     <div className="main">
       <Header />
       <NavbarLine />
       <Navbar />
       <NavbarLine />
+      <Duration activeIndex={activeIndex} setActiveIndex={setActiveIndex}/>
     </div>
   )
 }
