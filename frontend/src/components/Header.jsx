c0a345b9dc0ae77544037252e99b14a0af169299
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-04-05 13:41:17 +0300

    added Header component

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header () {
+  return (
+    <header>
+      <h1 className="title">Typing Speed Test</h1> 
+      <h3>Test Your Skills!</h3>
+    </header>
+  )
+}
+
+export default Header  
