be1ef31775f32bd4c43314c625bfa4b62cfb6a55
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-04-06 18:47:26 +0300

    added error if cannot fetch text from file

--- frontend/src/components/Words.jsx
+++ frontend/src/components/Words.jsx
@@ -3,13 +3,19 @@ import { loadRandomWords } from '../utils/textFileUtils'
 const letters = await loadRandomWords()
 
 function Words() {
-  return (
-    <div className="words">
-      {letters.map((letter, index) => {
-        return <span key={index}>{letter}</span>
-      })}
-    </div>
-  )
+  if (letters === null) {
+    return (
+      <span className="error-text">ERROR: failed to generate text</span>
+    )   
+  } else {
+    return (
+      <div className="words">
+        {letters.map((letter, index) => {
+          return <span key={index}>{letter}</span>
+        })}
+      </div>
+    )
+  }
 }
 
 export default Words
