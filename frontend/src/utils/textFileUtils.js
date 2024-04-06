462e353aede7d5f9bb840471ef0b7f601e310d4a
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-04-06 18:44:09 +0300

    created function that fetches text from file

---
+++ frontend/src/utils/textFileUtils.js
@@ -0,0 +1,15 @@
+export async function loadRandomWords() {
+  const newArr = []
+  try {
+    const res = await fetch('../../words.txt')
+    const t = await res.text()
+    const arr = t.split('\n')
+    for (let i = 0; i < 500; i++) {
+      newArr.push(arr[Math.floor(Math.random() * 1000)])
+    }
+    return newArr.join(' ').split('')
+  } catch (error) {
+    console.error(`error fetching words: ${error}`)
+    return null
+  }
+}
