ef9b574bbe846ab2e1f8c5753b4d69d7b41778b0
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-04-06 21:49:39 +0300

    created TestInfo component that display info text

---
+++ frontend/src/components/TestInfo.jsx
@@ -0,0 +1,10 @@
+function TestInfo() {
+  return (
+    <div className="info">
+      <img src="../../public/info.png" alt="info icon" />
+      The countdown will begin once you press Space key, then just type the words you see.
+    </div>
+  )
+}
+
+export default TestInfo
