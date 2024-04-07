b416fcc2a83ac7918e7432c29de64547dd03061d
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-04-07 19:47:54 +0300

    created WPM component

---
+++ frontend/src/components/WPM.jsx
@@ -0,0 +1,10 @@
+function WPM({ wpm }) {
+  return (
+    <div className="wpm stats-div">
+      <span className="num">43</span>
+      <span>words/min</span>
+    </div>
+  )
+}
+
+export default WPM
