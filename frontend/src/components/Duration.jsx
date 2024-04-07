1406e673f7eee4fe9c633f4cf87907defa689e52
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-04-07 14:56:52 +0300

    added keyPressed conditional to disable buttons once test is started

--- frontend/src/components/Duration.jsx
+++ frontend/src/components/Duration.jsx
@@ -1,11 +1,26 @@
-function Duration ({ activeIndex, setActiveIndex }) {
+function Duration({ activeIndex, setActiveIndex, keyPressed }) {
   return (
     <div className="duration">
       <p>Choose the duration of the test</p>
       <div className="duration-buttons">
-        <button className={`duration-button ${activeIndex === 1 ? 'active' : ''}`} onClick={() => setActiveIndex(1)}>1</button>
-        <button className={`duration-button ${activeIndex === 2 ? 'active' : ''}`} onClick={() => setActiveIndex(2)}>2</button>
-        <button className={`duration-button ${activeIndex === 3 ? 'active' : ''}`} onClick={() => setActiveIndex(3)}>3</button>
+        <button
+          className={`duration-button ${activeIndex === 1 ? 'active' : ''}`}
+          onClick={() => !keyPressed && setActiveIndex(1)}
+        >
+          1
+        </button>
+        <button
+          className={`duration-button ${activeIndex === 2 ? 'active' : ''}`}
+          onClick={() => !keyPressed && setActiveIndex(2)}
+        >
+          2
+        </button>
+        <button
+          className={`duration-button ${activeIndex === 3 ? 'active' : ''}`}
+          onClick={() => !keyPressed && setActiveIndex(3)}
+        >
+          3
+        </button>
         <p>{activeIndex === 1 ? 'minute' : 'minutes'}</p>
       </div>
     </div>
