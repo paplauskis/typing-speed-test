be7dc19902e94b32461b7e37335a84ce4d4ef436
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-04-07 19:47:21 +0300

    created CPM component

---
+++ frontend/src/components/CPM.jsx
@@ -0,0 +1,10 @@
+function CPM({ cpm }) {
+  return (
+    <div className="cpm stats-div">
+      <span className="num">234</span>
+      <span>chars/min</span>
+    </div>
+  )
+}
+
+export default CPM
