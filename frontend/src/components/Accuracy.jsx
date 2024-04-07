1445a93324eea6ab0d5ffe895ffe02bac8a00e8e
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-04-07 19:46:38 +0300

    created Accuracy component

---
+++ frontend/src/components/Accuracy.jsx
@@ -0,0 +1,10 @@
+function Accuracy({ accuracy }) {
+  return (
+    <div className="accuracy stats-div">
+      <span className="num">234</span>
+      <span>accuracy %</span>
+    </div>
+  )
+}
+
+export default Accuracy
