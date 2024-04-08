157c65120b8a16e390ac74e25b13c608392e2f8c
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-04-08 16:02:07 +0300

    added wordsPerMinute state, imported calculateWPM function

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,25 +10,27 @@ import Input from "./components/Input"
 import WPM from "./components/WPM"
 import CPM from "./components/CPM"
 import Accuracy from "./components/Accuracy"
-import { checkPressed } from "./utils/test"
+import checkPressed from "./utils/test"
+import calculateWPM from "./utils/calculateWPM"
 import { useState, useEffect } from "react"
 
 function App() {
   const [activeIndex, setActiveIndex] = useState(1)
   const [keyPressed, setKeyPressed] = useState(false)
   const [accuracy, setAccuracy] = useState('0%')
+  const [wordsPerMinute, setWordsPerMinute] = useState(0)
 
   useEffect(() => {
     const handlePress = (e) => {
       if (e.key === ' ') setKeyPressed(true)
       if (setKeyPressed) {
         setAccuracy(checkPressed(e))
+        setWordsPerMinute(calculateWPM())
       }
     }
     window.addEventListener('keydown', handlePress)
     return () => window.removeEventListener('keydown', handlePress)
   }, [])
-
   return (
     <>
       <Header />
@@ -40,7 +42,7 @@ function App() {
         <div className="test-stats">
           {!keyPressed && <TestInfo/>}
           {keyPressed && <Clock minutes={activeIndex}/>}
-          {keyPressed && <WPM />}
+          {keyPressed && <WPM wordsPerMinute={wordsPerMinute}/>}
           {keyPressed && <CPM />}
           {keyPressed && <Accuracy accuracy={accuracy}/>}
           {keyPressed && <Input />}
