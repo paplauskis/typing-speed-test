e6f47bead1060de8e5fe5569c77ae53d87c296dd
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-04-08 16:57:59 +0300

    created function that calculates words per minute

---
+++ frontend/src/utils/calculateCPM.js
@@ -0,0 +1,15 @@
+import { getTimeLeft, getTotalTime } from "./getTime"
+
+function calculateCPM() {
+  const tl = document.querySelectorAll('.typed')
+  const typedLetters = [...tl]
+  const totalTypedLetters = typedLetters.length
+  const timeLeft = getTimeLeft()
+  let totalTime = null
+  if (totalTime === null) totalTime = getTotalTime()
+  const timeElapsed = totalTime - timeLeft
+  const cpm = Math.floor(totalTypedLetters / timeElapsed * 60)
+  return cpm
+}
+
+export default calculateCPM
