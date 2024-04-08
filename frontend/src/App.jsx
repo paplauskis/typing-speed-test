405954d9d474e6d3826865ca03bed40131d26b7e
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-04-08 17:58:06 +0300

    added testStopped state that removes Duration and SpeedTestContainer components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,32 @@
-import Header from "./components/Header"
-import Navbar from "./components/Navbar"
-import NavbarLine from "./components/NavbarLine"
-import Duration from "./components/Duration"
-import SpeedTestContainer from "./components/SpeedTestContainer"
-import Words from "./components/Words"
-import Clock from "./components/Clock"
-import TestInfo from "./components/TestInfo"
-import Input from "./components/Input"
-import WPM from "./components/WPM"
-import CPM from "./components/CPM"
-import Accuracy from "./components/Accuracy"
-import checkPressed from "./utils/test"
-import calculateWPM from "./utils/calculateWPM"
-import calculateCPM from "./utils/calculateCPM"
-import { useState, useEffect } from "react"
+import Header from './components/Header'
+import Navbar from './components/Navbar'
+import NavbarLine from './components/NavbarLine'
+import Duration from './components/Duration'
+import SpeedTestContainer from './components/SpeedTestContainer'
+import Words from './components/Words'
+import Clock from './components/Clock'
+import TestInfo from './components/TestInfo'
+import Input from './components/Input'
+import WPM from './components/WPM'
+import CPM from './components/CPM'
+import Accuracy from './components/Accuracy'
+import checkPressed from './utils/test'
+import calculateWPM from './utils/calculateWPM'
+import calculateCPM from './utils/calculateCPM'
+import { useState, useEffect } from 'react'
 
 function App() {
   const [activeIndex, setActiveIndex] = useState(1)
-  const [gameStarted, setGameStarted] = useState(false)
+  const [testStarted, setGameStarted] = useState(false)
   const [accuracy, setAccuracy] = useState('0%')
   const [wordsPerMinute, setWordsPerMinute] = useState(0)
   const [charsPerMinute, setCharsPerMinute] = useState(0)
+  const [testStopped, setTestStopped] = useState(false)
+
+  const stopTest = () => {
+    console.log('test over')
+    setTestStopped(true)
+  }
 
   useEffect(() => {
     const handlePress = (e) => {
@@ -40,18 +46,28 @@ function App() {
       <NavbarLine />
       <Navbar />
       <NavbarLine />
-      <Duration activeIndex={activeIndex} setActiveIndex={setActiveIndex} keyPressed={gameStarted} />
-      <SpeedTestContainer>
-        <div className="test-stats">
-          {!gameStarted && <TestInfo/>}
-          {gameStarted && <Clock minutes={activeIndex}/>}
-          {gameStarted && <WPM wordsPerMinute={wordsPerMinute}/>}
-          {gameStarted && <CPM charsPerMinute={charsPerMinute}/>}
-          {gameStarted && <Accuracy accuracy={accuracy}/>}
-          {gameStarted && <Input />}
-        </div>
-        <Words />
-      </SpeedTestContainer>
+      {!testStopped && (
+        <>
+          <Duration
+            activeIndex={activeIndex}
+            setActiveIndex={setActiveIndex}
+            keyPressed={testStarted}
+          />
+          <SpeedTestContainer>
+            <div className="test-stats">
+              {!testStarted && <TestInfo />}
+              {testStarted && (
+                <Clock minutes={activeIndex} stopTest={stopTest} />
+              )}
+              {testStarted && <WPM wordsPerMinute={wordsPerMinute} />}
+              {testStarted && <CPM charsPerMinute={charsPerMinute} />}
+              {testStarted && <Accuracy accuracy={accuracy} />}
+              {testStarted && <Input />}
+            </div>
+            <Words />
+          </SpeedTestContainer>
+        </>
+      )}
     </>
   )
 }
