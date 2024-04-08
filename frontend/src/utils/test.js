531f2d21d47a0ab79df8323e543225751c3355a6
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-04-08 11:52:05 +0300

    added condition to backspace and fix typos

--- frontend/src/utils/test.js
+++ frontend/src/utils/test.js
@@ -2,15 +2,23 @@ let gameStarted = false
 let charsTyped = 0
 
 export function checkPressed(e) {
-  if (gameStarted) {
+  if (e.key === 'Backspace' && gameStarted) {
+    charsTyped--
     const letter = document.getElementById(`${charsTyped}`)
-    if (letter.textContent === e.key) {
-      letter.className = 'correct'
-    } else {
-      letter.className = 'wrong'
+    letter.classList.remove('current')
+    letter.classList.remove('wrong')
+    letter.classList.remove('correct')
+  } else {
+    if (gameStarted) {
+      const letter = document.getElementById(`${charsTyped}`)
+      if (letter.textContent === e.key) {
+        letter.className = 'correct'
+      } else {
+        letter.className = 'wrong'
+      }
+      letter.classList.add('current')
+      charsTyped++
     }
-    letter.classList.add('current')
-    charsTyped++
   }
   if (!gameStarted && !charsTyped && e.key === ' ') {
     gameStarted = true
