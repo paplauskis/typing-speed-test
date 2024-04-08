532b39a6ab0cf7b23f275b5541fe63fced22dde1
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-04-08 21:27:56 +0300

    created functions that count total wrong & correct chars

---
+++ frontend/src/utils/calculateChars.js
@@ -0,0 +1,21 @@
+function calculateChars() {
+  const correctChars = getCorrectChars()
+  const wrongChars = getWrongChars()
+  return {correctChars, wrongChars}
+}
+
+function getCorrectChars() {
+  const cl = document.querySelectorAll('.correct')
+  const cletters = [...cl]
+  const correctLetters = cletters.length
+  return correctLetters
+}
+
+function getWrongChars() {
+  const wl = document.querySelectorAll('.wrong')
+  const wletters = [...wl]
+  const wrongLetters = wletters.length
+  return wrongLetters
+}
+
+export default calculateChars
