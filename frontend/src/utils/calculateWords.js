c0116936833747091f83e3927192aa0f2a6bd2dd
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-04-09 09:53:59 +0300

    created function that counts correct word amount

--- frontend/src/utils/calculateWords.js
+++ frontend/src/utils/calculateWords.js
@@ -1,5 +1,7 @@
 function calculateWords() {
   const count = getWordCount()
+  const correctWords = getCorrectWords()
+  const wrongWords = count - correctWords
   return {correctWords, wrongWords}
 }
 
@@ -12,6 +14,24 @@ function getWordCount() {
       count++
   })
   return count
+}
+
+function getCorrectWords() {
+  const tw = document.querySelectorAll('.typed')
+  const typedLetters = [...tw]
+  let correctWordCount = 0
+  let wrong = false
+  typedLetters.forEach(letter => {
+    if (letter.classList[0] === 'wrong') {
+      wrong = true
+    }
+    if (letter.textContent === ' ' && wrong === false) {
+      correctWordCount++
+    } else if (letter.textContent === ' ' && wrong === true) {
+      wrong = false
+    }
+  })
+  return correctWordCount
 }
 
 export default calculateWords
