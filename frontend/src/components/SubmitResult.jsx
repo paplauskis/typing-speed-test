bfc77c3253d9111f53a7ad17bafbc9699e0018b2
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-04-09 14:42:55 +0300

    created SubmitResult component that submitstest results

---
+++ frontend/src/components/SubmitResult.jsx
@@ -0,0 +1,17 @@
+function SubmitResult() {
+  return (
+    <div className="submit-result">
+      <span className="submit-text">
+        Submit your result to compare yourself to other users
+      </span>
+      <form action="/ranking" method="POST">
+        <div className="form">
+        <input id="username" name="username" type="text" />
+        <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default SubmitResult
