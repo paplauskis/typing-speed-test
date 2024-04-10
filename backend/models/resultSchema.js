c377a61e7f534bf79d10bd65744b752bb1d7123d
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-04-10 11:09:10 +0300

    added username property

--- backend/models/resultSchema.js
+++ backend/models/resultSchema.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const resultSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+  },
   score: {
     type: Number,
     required: true,
