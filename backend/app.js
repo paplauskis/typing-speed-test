ddcf535c97c171704bbe0d008fdd4248d5b87d96
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-04-10 10:50:24 +0300

    added code that connects to database

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,23 @@
 require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose')
 const indexRouter = require('./routes/index')
 
 const app = express()
+const mongoDB = process.env.MONGO_DB_URI
 
+app.use(express.json())
 app.use('/', indexRouter)
 
-app.listen(process.env.PORT, () => {
-  console.log(`listening on port ${process.env.PORT}`)
-})
+main().catch((err) => console.log(err))
+async function main() {
+  await mongoose.connect(mongoDB)
+    .then(() => {
+      app.listen(process.env.PORT, () => {
+      console.log(`listening on port ${process.env.PORT}`)
+      })
+    })
+    .catch(error => {
+      console.log(error)
+    })
+}
