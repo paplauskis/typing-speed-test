b438c1e25bab5041921b4ea3c239c343e60d66ff
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-04-10 12:14:11 +0300

    moved page content to pages folder, this file will track links

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,100 +1,21 @@
-import Header from './components/Header'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
-import NavbarLine from './components/NavbarLine'
-import Duration from './components/Duration'
-import SpeedTestContainer from './components/SpeedTestContainer'
-import Words from './components/Words'
-import Clock from './components/Clock'
-import TestInfo from './components/TestInfo'
-import Input from './components/Input'
-import WPM from './components/WPM'
-import CPM from './components/CPM'
-import Accuracy from './components/Accuracy'
-import TestStats from './components/TestStats'
-import checkPressed from './utils/test'
-import calculateWPM from './utils/calculateWPM'
-import calculateCPM from './utils/calculateCPM'
-import calculateChars from './utils/calculateChars'
-import calculateWords from './utils/calculateWords'
-import scrollWords from './utils/scrollWords'
-import { useState, useEffect } from 'react'
-
+import Home from './pages/Home'
+import PCRankings from './pages/PCRankings'
+import MobileRankings from './pages/MobileRankings'
 function App() {
-  const [activeIndex, setActiveIndex] = useState(1)
-  const [testStarted, setGameStarted] = useState(false)
-  const [accuracy, setAccuracy] = useState('0%')
-  const [wordsPerMinute, setWordsPerMinute] = useState(0)
-  const [charsPerMinute, setCharsPerMinute] = useState(0)
-  const [testStopped, setTestStopped] = useState(false)
-  const [correctChars, setCorrectChars] = useState(0)
-  const [correctWords, setCorrectWords] = useState(0)
-  const [wrongChars, setWrongChars] = useState(0)
-  const [wrongWords, setWrongWords] = useState(0)
-
-  const stopTest = () => {
-    setCorrectChars(calculateChars().correctChars)
-    setCorrectWords(calculateWords().correctWords)
-    setWrongChars(calculateChars().wrongChars)
-    setWrongWords(calculateWords().wrongWords)
-    setTestStopped(true)
-  }
-
-  useEffect(() => {
-    const handlePress = (e) => {
-      if (e.key === ' ') setGameStarted(true)
-      if (setGameStarted) {
-        setAccuracy(checkPressed(e))
-        setWordsPerMinute(calculateWPM())
-        setCharsPerMinute(calculateCPM())
-        scrollWords()
-      }
-    }
-
-    if (!testStopped) {
-      window.addEventListener('keydown', handlePress)
-    }
-    return () => window.removeEventListener('keydown', handlePress)
-  }, [testStopped])
   return (
     <>
-      <Header />
-      <NavbarLine />
-      <Navbar />
-      <NavbarLine />
-      {!testStopped && (
-        <>
-          <Duration
-            activeIndex={activeIndex}
-            setActiveIndex={setActiveIndex}
-            keyPressed={testStarted}
-          />
-          <SpeedTestContainer>
-            <div className="test-stats">
-              {!testStarted && <TestInfo />}
-              {testStarted && (
-                <Clock minutes={activeIndex} stopTest={stopTest} />
-              )}
-              {testStarted && <WPM wordsPerMinute={wordsPerMinute} />}
-              {testStarted && <CPM charsPerMinute={charsPerMinute} />}
-              {testStarted && <Accuracy accuracy={accuracy} />}
-              {testStarted && <Input />}
-            </div>
-            <Words />
-          </SpeedTestContainer>
-        </>
-      )}
-      {testStopped && (
-        <TestStats
-          accuracy={accuracy}
-          wpm={wordsPerMinute}
-          cpm={charsPerMinute}
-          correctChars={correctChars}
-          wrongChars={wrongChars}
-          correctWords={correctWords}
-          wrongWords={wrongWords}
-          minutes={activeIndex}
-        />
-      )}
+      <BrowserRouter>
+        <div className="pages">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/pc-rankings" element={<PCRankings />} />
+            <Route path="/mobile-rankings" element={<MobileRankings />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </>
   )
 }
