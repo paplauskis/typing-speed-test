5752153906927082aabe79456bb8a8b5122262f7
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-04-10 12:15:30 +0300

    imported Header and NavbarLine

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,18 @@
+import Header from './Header'
+import NavbarLine from './NavbarLine'
+
 function Navbar() {
   return (
-    <div className="navbar">
-      <button className="nav-option">Typing Test</button>
-      <button className="nav-option">Standard Keyboard Rankings</button>
-      <button className="nav-option">Virtual Keyboard Rankings</button>
-    </div>
+    <>
+      <Header />
+      <NavbarLine />
+      <div className="navbar">
+        <button className="nav-option">Typing Test</button>
+        <button className="nav-option">Standard Keyboard Rankings</button>
+        <button className="nav-option">Virtual Keyboard Rankings</button>
+      </div>
+      <NavbarLine />
+    </>
   )
 }
 
