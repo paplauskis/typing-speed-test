07246bdb247e5a0820676368dedd822a5a2fb5a4
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-04-12 11:36:49 +0300

    removed a few unneeded imports

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,3 @@
-import Header from '../components/Header'
-import Navbar from '../components/Navbar'
-import NavbarLine from '../components/NavbarLine'
 import Duration from '../components/Duration'
 import SpeedTestContainer from '../components/SpeedTestContainer'
 import Words from '../components/Words'
