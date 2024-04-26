8dd1550e4eb125244cedb945ebdc5e3c0d478922
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-04-26 13:31:11 +0300

    changed text

--- frontend/src/components/TestInfo.jsx
+++ frontend/src/components/TestInfo.jsx
@@ -2,7 +2,7 @@ function TestInfo() {
   return (
     <div className="info">
       <img src="../../public/info.png" alt="info icon" />
-      The countdown will begin once you press Space key, then just type the words you see.
+      The countdown will begin once you start typing.
     </div>
   )
 }
