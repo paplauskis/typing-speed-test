dbb3408a0b1ede367735aecaea6f387da92b1b4f
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-04-26 11:50:13 +0300

    added charsTyped as a parameter and removed gameStarted global var
    this was done because when test page was visited from another page the charsTyped wasn't refreshed because it was a global variable

--- frontend/src/utils/test.js
+++ frontend/src/utils/test.js
@@ -1,36 +1,25 @@
 import calculateAccuracy from './calculateAccuracy'
 
-let gameStarted = false
-let charsTyped = 0
-
-function checkPressed(e) {
-  if (e.key === 'Backspace' && gameStarted) {
-    charsTyped--
-    const letter = document.getElementById(`${charsTyped}`)
+function checkPressed(e, charsTyped) {
+  if (e.key === 'Backspace') {
+    const letter = document.getElementById(`${charsTyped - 1}`)
     letter.classList.remove('typed')
     letter.classList.remove('wrong')
     letter.classList.remove('correct')
     letter.textContent === '_' ? (letter.textContent = ' ') : letter.textContent
     return calculateAccuracy()
   } else {
-    if (gameStarted) {
-      const letter = document.getElementById(`${charsTyped}`)
-      if (letter.textContent === e.key) {
-        letter.className = 'correct'
-      } else {
-        letter.className = 'wrong'
-        letter.textContent === ' '
-          ? (letter.textContent = '_')
-          : letter.textContent
-      }
-      letter.classList.add('typed')
-      charsTyped++
-      return calculateAccuracy()
+    const letter = document.getElementById(`${charsTyped}`)
+    if (letter.textContent === e.key) {
+      letter.className = 'correct'
+    } else {
+      letter.className = 'wrong'
+      letter.textContent === ' '
+        ? (letter.textContent = '_')
+        : letter.textContent
     }
-  }
-  if (!gameStarted && !charsTyped && e.key === ' ') {
-    gameStarted = true
-    return '0%'
+    letter.classList.add('typed')
+    return calculateAccuracy()
   }
 }
 
