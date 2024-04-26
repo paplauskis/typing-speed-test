720d003a33c7eb019ffa1dd3f11742cf4e96dbe8
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-04-26 13:46:14 +0300

    added separate finding for different length tests

--- backend/controllers/resultController.js
+++ backend/controllers/resultController.js
@@ -40,12 +40,30 @@ exports.home_create_result_post = async (req, res) => {
 
 exports.pc_rankings_get = async (req, res) => {
   try {
-    const results = await Result.find({ device: 'pc' }).sort({
-      score: -1,
-      accuracy: -1,
-      correctChars: -1,
-    })
-    res.status(200).json(results)
+    const oneMinuteResults = await Result.find({ device: 'pc', minutes: 1 })
+      .sort({
+        score: -1,
+        accuracy: -1,
+        correctChars: -1,
+      })
+      .limit(100)
+    const twoMinuteResults = await Result.find({ device: 'pc', minutes: 2 })
+      .sort({
+        score: -1,
+        accuracy: -1,
+        correctChars: -1,
+      })
+      .limit(100)
+    const threeMinuteResults = await Result.find({ device: 'pc', minutes: 3 })
+      .sort({
+        score: -1,
+        accuracy: -1,
+        correctChars: -1,
+      })
+      .limit(100)
+    res
+      .status(200)
+      .json({ oneMinuteResults, twoMinuteResults, threeMinuteResults })
   } catch (error) {
     res.status(500).json({ error: error })
   }
